4c18c92a34f04eb544d3e5c24a0bd5f32c9bdb0a
Strangestring <1814232115@qq.com>
2019-08-07 21:48:15 +0800

    Create headers_pool.py

---
+++ aiofetch/headers_pool.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author : strangestring
+# github : https://github.com/strangestring
+
+
+# headers pool for random selection
+HEADERS_POOL = [{
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'cookie': <YOUR COOKIE HERE>}, ]
